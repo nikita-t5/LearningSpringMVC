22e6418febb5533b9522fd58c907f1d8760521d0
nikita-t5 <nickita.tashirev@yandex.ru>
2021-04-27 21:35:52 +0300

    RequestParam in Controller

--- spring-mvc-app1/src/main/java/ru/simpleSpringMVCApp/controllers/FirstController.java
+++ spring-mvc-app1/src/main/java/ru/simpleSpringMVCApp/controllers/FirstController.java
@@ -2,12 +2,16 @@ package ru.simpleSpringMVCApp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 @RequestMapping("/f")
 public class FirstController {
+
     @GetMapping("/hello") //URL get request
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello " + name + " " + surname);
         return "first/hello"; //location views
     }
 
